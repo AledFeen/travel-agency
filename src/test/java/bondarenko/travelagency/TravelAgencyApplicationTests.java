75a4769b6a299f5b30341d1479bde5f78afec3f9
AledFeen <vovabond849@gmail.com>
2023-09-23 18:36:08 +0300

    start

---
+++ src/test/java/bondarenko/travelagency/TravelAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package bondarenko.travelagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
